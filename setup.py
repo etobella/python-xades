2b9d118d68389e0f64d642e360b9b27a45de7cfa
Enric Tobella <etobella@creublanca.es>
2021-11-20 12:42:30 +0100

    [FIX] license

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
+import pathlib
+
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
-install_requires = ["pytz", "xmlsig"]
-
-tests_require = [
-    "freezegun==0.3.8",
-    "mock==2.0.0",
-    "pretend==1.0.8",
-    "pytest-cov==2.5.1",
-    "pytest==3.1.3",
-    "requests_mock>=0.7.0",
-    "PyOpenSSL",
-    "isort==4.2.5",
-    "flake8==3.3.0",
-    "flake8-blind-except==0.1.1",
-    "flake8-debugger==1.4.0",
-    "flake8-imports==0.1.1",
-]
+with pathlib.Path("requirements.txt").open() as requirements_txt:
+    install_requires = [
+        str(requirement) for requirement in parse_requirements(requirements_txt)
+    ]
+with pathlib.Path("test-requirements.txt").open() as requirements_txt:
+    test_requires = [
+        str(requirement) for requirement in parse_requirements(requirements_txt)
+    ]
 
 
 setup(
@@ -27,8 +22,8 @@ setup(
     author_email="etobella@creublanca.es",
     url="http://github.com/etobella/python-xades",
     install_requires=install_requires,
-    tests_require=tests_require,
-    extras_require={"test": tests_require},
+    tests_require=test_requires,
+    extras_require={"test": test_requires},
     entry_points={},
     package_dir={"": "src"},
     packages=find_packages("src"),
@@ -36,7 +31,7 @@ setup(
     license="AGPL-3",
     classifiers=[
         "Development Status :: 4 - Beta",
-        "GNU Affero General Public License v3 or later (AGPLv3+)",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
